15003d6516e7f23a1bd5f030cab9905adc6fd0dc
Mian Dawood Bin Rafay <dawoodbinrafaydbr@gmail.com>
2025-07-09 23:23:48 +0500

    APP

---
+++ src/data/Products.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    id: 1,
+    name: "Snake Plant",
+    category: "Low Light",
+    price: 1200,
+    image: "/plants/snake-plant.jpg",
+  },
+  {
+    id: 2,
+    name: "Peace Lily",
+    category: "Flowering",
+    price: 1500,
+    image: "/plants/peace-lily.jpg",
+  },
+  {
+    id: 3,
+    name: "Aloe Vera",
+    category: "Succulent",
+    price: 900,
+    image: "/plants/aloe-vera.jpg",
+  },
+  {
+    id: 4,
+    name: "Monstera",
+    category: "Tropical",
+    price: 1800,
+    image: "/plants/monstera.jpg",
+  },
+  {
+    id: 5,
+    name: "ZZ Plant",
+    category: "Low Light",
+    price: 1100,
+    image: "/plants/zz-plant.jpg",
+  },
+  {
+    id: 6,
+    name: "Spider Plant",
+    category: "Air Purifier",
+    price: 950,
+    image: "/plants/spider-plant.jpg",
+  },
+];
+
+export default products;
