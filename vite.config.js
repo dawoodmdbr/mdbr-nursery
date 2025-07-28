7c94692dccf524fe6cc5d8be3489186243306fa3
Mian Dawood Bin Rafay <dawoodbinrafaydbr@gmail.com>
2025-07-28 20:36:14 +0500

    ...

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mdbr-nursery/', 
+  base: '/mdbr-nursery/', // <-- IMPORTANT: match your repo name
   plugins: [react()],
 })
